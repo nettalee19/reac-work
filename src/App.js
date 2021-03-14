83b743d2bc4c2ed2c9de70c15370625b912906f2
Netta Avidan <nettavidan19@gmail.com>
2021-03-14 15:59:41 +0200

    3.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
-import Basic22 from './Components/Basic2.2/Basic2.2';
+// import Basic22 from './Components/Basic2.2/Basic2.2';
+import Basic3 from './Components/Basic3.1/Basic3.1';
 
 function App() {
   return (
     <div className="App">
-      <Basic22></Basic22>
+      {/* <Basic22></Basic22> */}
+      <Basic3></Basic3>
 
     </div>
   );
