48dbbc8bd303db6318b2658e369886bd817ee13d
Netta Avidan <nettavidan19@gmail.com>
2021-03-14 19:03:16 +0200

    2.1

---
+++ src/Components/Basic2.1/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+// import Basic22 from './Components/Basic2.2/Basic2.2';
+import Basic3 from './Components/Basic3.1/Base';
+// import Basic321 from './Components/Basic3.2/Basic3.21';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Basic22></Basic22> */}
+      <Basic3></Basic3>
+
+    </div>
+  );
+}
+
+export default App;
