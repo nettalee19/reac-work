bcac540511784e22405385d88baf8aef0fd9a49d
Netta Avidan <nettavidan19@gmail.com>
2021-03-15 12:24:41 +0200

    buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
 // import Basic22 from './Components/Basic2.2/Basic2.2';
-import Basic3 from './Components/Basic3.1/Base';
+// import Basic3 from './Components/Basic3.1/Base';
 // import Basic321 from './Components/Basic3.2/Basic3.21';
+import Buttons from './Components/Basic3.2/Buttons';
 
 function App() {
   return (
     <div className="App">
       {/* <Basic22></Basic22> */}
-      <Basic3></Basic3>
+      {/* <Basic3></Basic3> */}
+      <Buttons></Buttons>
 
     </div>
   );
---
+++ src/Components/Basic4.1/ButtonsCon.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+const Quiz = () =>{
+    return (
+        <div>
+            How do You like front end?
+            <div>
+                <div>How much you love frond end?</div>
+                <div>How much you love frond end?</div>
+            </div>
+            <div>
+                <div>What is you favorite frond end feature?</div>
+                <div> <input></input> </div>
+            </div>
+        </div> 
+    )
+}
+
+export default Quiz;
