17c7fbae7693045fb640e7d4cc550d44380fd1b9
Netta Avidan <nettavidan19@gmail.com>
2021-03-24 17:44:36 +0200

    16.1

--- src/Components/Basic16.1/ProductDetail.js
+++ src/Components/Basic16.1/ProductDetail.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
+// import Products from './Products';
 import store from './store'
+// import {Link} from 'react-router-dom'
 
 export default class ProductPage extends Component {
-    state= {id: this.props.id}
+    // state= {id: this.props.id}
+    constructor(props){
+        super(props);
+        this.state ={
+            id:  props.match.params.id,//what was sent by the father
+            products: store,
+        }
+        
+    }
     
     render() {
         return (
             <div>
-               <p>{this.state.id}</p>
+               <p>{this.state.products[this.state.id-1].title}</p>
+               <img src={this.state.products[this.state.id-1].imageUrl} alt=""/>
+               <p>{this.state.products[this.state.id-1].price}$</p>
+               <p>{this.state.products[this.state.id-1].size}</p>
             </div>
+            // <div>
+            //     <Link to="/products">Products</Link>
+
+            // </div>
         )
     }
 }
--- src/Components/Basic16.1/Products.js
+++ src/Components/Basic16.1/Products.js
@@ -4,18 +4,27 @@ import ProductDetail from "./ProductDetail"
 import {BrowserRouter, Link, Route} from 'react-router-dom'
 
 export default class Products extends Component {
-    state= {store}
+    // state= {store}
+    constructor(props){
+        super(props);
+        this.state = {
+            products: [], 
+        }
+    }
 
     componentDidMount(){
-
+        this.setState({products: store})
     }
 
     render() {
         return (
             <div>
                 <BrowserRouter>
-                    <div>
-                        <Link to="/products/1">Stylish hat </Link> 
+                    <div><h3>On Sale:</h3>
+                    {this.state.products.map(((product)=>{
+                        return <Link to={`/products/${product.id}`} key={product.id}> {product.title}   </Link>
+                    }))}
+                        {/* <Link to="/products/1">Stylish hat </Link>  */}
                         {/* <Link to="/products/2">Beautiful Jacket </Link> 
                         <Link to="/products/3">Fashionable Jeans </Link> 
                         <Link to="/products/4">Smart tie </Link>  */}
